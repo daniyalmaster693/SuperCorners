13c5ebd8349003246aa2f9e7e728cf247c58c9f8
Daniyal Master <daniyalmaster693@gmail.com>
2025-05-22 12:47:02 -0400

    Initial Commit

---
+++ SuperCorners/SuperCornersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCornersApp.swift
+//  SuperCorners
+//
+//  Created by Daniyal Master on 2025-05-22.
+//
+
+import SwiftUI
+
+@main
+struct SuperCornersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SuperCornersTests/SuperCornersTests.swift
@@ -0,0 +1,17 @@
+//
+//  SuperCornersTests.swift
+//  SuperCornersTests
+//
+//  Created by Daniyal Master on 2025-05-22.
+//
+
+import Testing
+@testable import SuperCorners
+
+struct SuperCornersTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
