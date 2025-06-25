d4ba6b6eaab8fac3f407a4c3afbf3e76dcec345d
Daniyal Master <daniyalmaster693@gmail.com>
2025-06-25 15:14:18 -0400

    Updated the disable in full screen toggle to control whether actions are triggered in full screen

--- SuperCorners/Actions/CornerMousePosition.swift
+++ SuperCorners/Actions/CornerMousePosition.swift
@@ -11,8 +11,21 @@ private var lastCorner: CornerPosition.Corner?
 
 func getCornerMousePosition() {
     @AppStorage("triggerSensitivity") var triggerSensitivity = 5.0
+    @AppStorage("disableInFullScreen") var disableInFullScreen = false
 
     let mousePosition: NSPoint = NSEvent.mouseLocation
+
+    if disableInFullScreen {
+        for window in NSApplication.shared.windows {
+            if window.styleMask.contains(.fullScreen) {
+                let windowFrame = window.frame
+                if windowFrame.contains(mousePosition) {
+                    lastCorner = nil
+                    return
+                }
+            }
+        }
+    }
 
     for screen in NSScreen.screens {
         let corners: [CornerPosition.Corner] = [.topLeft, .topRight, .bottomLeft, .bottomRight, .top, .left, .right, .bottom]
