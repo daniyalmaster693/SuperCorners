e62727b7c402dd28da9215c7e1dd445af89ee325
Daniyal Master <daniyalmaster693@gmail.com>
2025-07-01 13:14:25 -0400

    Added a new system that allows you to use a modifier key to activate a corner/zone

--- SuperCorners/Actions/CornerHotkey.swift
+++ SuperCorners/Actions/CornerHotkey.swift
@@ -12,6 +12,40 @@ var localCornerMonitor: Any?
 var globalCornerMonitor: Any?
 
 func activateCornerHotkey() {
+    // Activation Using Modifier Key
+
+    var modifierKeyPressed = false
+
+    NSEvent.addLocalMonitorForEvents(matching: [.flagsChanged]) { event in
+        let modifierPressed = event.modifierFlags.contains(.command)
+
+        if modifierPressed, !modifierKeyPressed {
+            modifierKeyPressed = true
+            localCornerMonitor = NSEvent.addLocalMonitorForEvents(matching: [.mouseMoved]) { event in
+                getCornerMousePosition()
+                return event
+            }
+
+            globalCornerMonitor = NSEvent.addGlobalMonitorForEvents(matching: [.mouseMoved]) { _ in
+                getCornerMousePosition()
+            }
+        } else if !modifierPressed, modifierKeyPressed {
+            modifierKeyPressed = false
+            if let local = localCornerMonitor {
+                NSEvent.removeMonitor(local)
+                localCornerMonitor = nil
+            }
+
+            if let global = globalCornerMonitor {
+                NSEvent.removeMonitor(global)
+                globalCornerMonitor = nil
+            }
+        }
+        return event
+    }
+
+    // Activation Using Keyboard Shortcut
+
     if KeyboardShortcuts.Name.cornerActivation.shortcut == nil {
         KeyboardShortcuts.setShortcut(.init(.space, modifiers: [.control, .shift]), for: .cornerActivation)
     }
