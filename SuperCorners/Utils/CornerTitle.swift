56ac88cb737dcbaccf0fd74cf186dc2035f1436c
Daniyal Master <daniyalmaster693@gmail.com>
2025-12-19 16:15:48 -0500

    Updated input titles for buttons to support more template actions

--- SuperCorners/Utils/CornerTitle.swift
+++ SuperCorners/Utils/CornerTitle.swift
@@ -14,11 +14,50 @@ func titleForCorner(_ corner: CornerPosition.Corner) -> String {
 
     let input = UserDefaults.standard.string(forKey: "cornerInput_\(corner.rawValue)")
 
-    if action.id == "96",
-       let input,
-       !input.isEmpty
-    {
-        return input
+    switch action.id {
+    case "4":
+        if let input, let url = URL(string: input), let host = url.host {
+            return host.prefix(1).uppercased() + host.dropFirst()
+        }
+
+    case "5":
+        if let input, !input.isEmpty {
+            let appURL = URL(fileURLWithPath: input)
+            let appName = appURL.deletingPathExtension().lastPathComponent
+            return "Toggle \(appName.capitalized)"
+        }
+
+    case "6":
+        if let input, !input.isEmpty {
+            return input.capitalized
+        }
+
+    case "7":
+        if let input, !input.isEmpty {
+            let url = URL(fileURLWithPath: input)
+            let lastComponent = url.lastPathComponent
+            return "Open \(lastComponent.prefix(1).uppercased() + lastComponent.dropFirst())"
+        }
+
+    case "50":
+        if let input, !input.isEmpty {
+            let url = URL(fileURLWithPath: input)
+            let lastComponent = url.lastPathComponent
+            return "Open \(lastComponent.prefix(1).uppercased() + lastComponent.dropFirst())"
+        }
+
+    case "74":
+        if let input, !input.isEmpty {
+            return "Countdown to \(input.capitalized)"
+        }
+
+    case "96":
+        if let input, !input.isEmpty {
+            return input.capitalized
+        }
+
+    default:
+        break
     }
 
     return action.title
