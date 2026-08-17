75157a839955e2e5668ed93b4bc6491ca93f2b73
Daniyal Master <daniyalmaster693@gmail.com>
2026-08-17 08:42:51 -0400

    Updated app version and walkthrough descriptions

--- SuperCorners/AppDelgate.swift
+++ SuperCorners/AppDelgate.swift
@@ -23,10 +23,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
             tour.present(
                 pages: [
                     TourPage(imageName: "tour-welcome", title: "Welcome to SuperCorners", description: "Supercharge your Mac's Corners"),
-                    TourPage(imageName: "tour-zones", title: "More Ways to Trigger", description: "Go beyond the corners with 4 additional zones."),
-                    TourPage(imageName: "tour-actions", title: "Powerful Actions", description: "Trigger over 50 different actions from your triggers."),
+                    TourPage(imageName: "tour-zones", title: "More Ways to Trigger", description: "Go beyond the corners with 4 additional zones at each edge of your display."),
+                    TourPage(imageName: "tour-actions", title: "Powerful Actions", description: "Trigger over 50 different actions to enable powerful workflows."),
                     TourPage(imageName: "tour-activation", title: "Customizable Activation", description: "Optionally choose between modifier keys, keyboard shortcuts, hovering or clicking."),
-                    TourPage(imageName: "tour-accessibility", title: "Enable Accessibility Permissions", description: "Give SuperCorners accessibility permission."),
+                    TourPage(imageName: "tour-accessibility", title: "Enable Accessibility Permissions", description: "Give SuperCorners accessibility permission for the best experience."),
                     TourPage(imageName: "tour-default", title: "Disable Default Hot Corners", description: "Disable the built in hot corners feature for the best experience."),
                 ],
                 width: 900,
