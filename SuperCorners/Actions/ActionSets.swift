351cedcf6136d2ba4b4375c0a0bb1339a94ea7d9
Daniyal Master <daniyalmaster693@gmail.com>
2026-09-04 11:11:07 -0400

    Added placeholder action set actions

--- SuperCorners/Actions/ActionSets.swift
+++ SuperCorners/Actions/ActionSets.swift
@@ -8,10 +8,25 @@
 import AppKit
 import Foundation
 
+struct ActionAssignment: Codable {
+    var actionID: String
+    var input: String?
+}
+
 struct ActionSet: Codable, Identifiable {
     var id: UUID = .init()
     var name: String
     var targetBundleID: String?
+
+    var topLeft: ActionAssignment
+    var topRight: ActionAssignment
+    var bottomLeft: ActionAssignment
+    var bottomRight: ActionAssignment
+
+    var top: ActionAssignment
+    var left: ActionAssignment
+    var right: ActionAssignment
+    var bottom: ActionAssignment
 }
 
 final class ActionSetManager: ObservableObject {
@@ -20,15 +35,111 @@ final class ActionSetManager: ObservableObject {
     @Published var availableSets: [ActionSet] = [
         ActionSet(
             name: "Global Actions",
-            targetBundleID: nil
+            targetBundleID: nil,
+            topLeft: ActionAssignment(
+                actionID: "screenSaver",
+                input: nil
+            ),
+            topRight: ActionAssignment(
+                actionID: "sleepDisplay",
+                input: nil
+            ),
+            bottomLeft: ActionAssignment(
+                actionID: "lockScreen",
+                input: nil
+            ),
+            bottomRight: ActionAssignment(
+                actionID: "spotlightSearch",
+                input: nil
+            ),
+            top: ActionAssignment(
+                actionID: "spotlightApps",
+                input: nil
+            ),
+            left: ActionAssignment(
+                actionID: "missionControl",
+                input: nil
+            ),
+            right: ActionAssignment(
+                actionID: "applicationWindows",
+                input: nil
+            ),
+            bottom: ActionAssignment(
+                actionID: "notificationCenter",
+                input: nil
+            ),
         ),
         ActionSet(
             name: "Safari Actions",
-            targetBundleID: "com.apple.safari"
+            targetBundleID: "com.apple.safari",
+            topLeft: ActionAssignment(
+                actionID: "screenSaver",
+                input: nil
+            ),
+            topRight: ActionAssignment(
+                actionID: "sleepDisplay",
+                input: nil
+            ),
+            bottomLeft: ActionAssignment(
+                actionID: "toggleTheme",
+                input: nil
+            ),
+            bottomRight: ActionAssignment(
+                actionID: "toggleAwake",
+                input: nil
+            ),
+            top: ActionAssignment(
+                actionID: "spotlightApps",
+                input: nil
+            ),
+            left: ActionAssignment(
+                actionID: "missionControl",
+                input: nil
+            ),
+            right: ActionAssignment(
+                actionID: "applicationWindows",
+                input: nil
+            ),
+            bottom: ActionAssignment(
+                actionID: "notificationCenter",
+                input: nil
+            ),
         ),
         ActionSet(
             name: "Xcode Actions",
-            targetBundleID: "com.apple.dt.xcode"
+            targetBundleID: "com.apple.dt.xcode",
+            topLeft: ActionAssignment(
+                actionID: "screenSaver",
+                input: nil
+            ),
+            topRight: ActionAssignment(
+                actionID: "sleepDisplay",
+                input: nil
+            ),
+            bottomLeft: ActionAssignment(
+                actionID: "createNote",
+                input: nil
+            ),
+            bottomRight: ActionAssignment(
+                actionID: "createEvent",
+                input: nil
+            ),
+            top: ActionAssignment(
+                actionID: "spotlightApps",
+                input: nil
+            ),
+            left: ActionAssignment(
+                actionID: "missionControl",
+                input: nil
+            ),
+            right: ActionAssignment(
+                actionID: "applicationWindows",
+                input: nil
+            ),
+            bottom: ActionAssignment(
+                actionID: "notificationCenter",
+                input: nil
+            ),
         ),
     ]
 }
