a64c3332823a7033ce86473480c53c23f7de60d4
Daniyal Master <daniyalmaster693@gmail.com>
2026-09-07 09:14:11 -0400

    Added action set manager

---
+++ SuperCorners/Actions/ActionSetManager.swift
@@ -0,0 +1,27 @@
+//
+//  ActionSetManager.swift
+//  SuperCorners
+//
+//  Created by Daniyal Master on 2026-09-07.
+//
+
+import Foundation
+
+class ActionSetManager: ObservableObject {
+    static let shared = ActionSetManager()
+
+    @Published var actionSets: [ActionSet] = []
+
+//    private init() {}
+
+    func loadDefaultConfig() {
+        guard let url = Bundle.main.url(forResource: "config", withExtension: "json"),
+              let data = try? Data(contentsOf: url),
+              let config = try? JSONDecoder().decode(ActionsConfig.self, from: data)
+        else {
+            fatalError("Failed to load or decode default-config.json")
+        }
+
+        actionSets = config.actionSets
+    }
+}
