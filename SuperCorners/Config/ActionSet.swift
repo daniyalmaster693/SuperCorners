2ef913c8583b7df6ed166f176e916f0337a32b33
Daniyal Master <daniyalmaster693@gmail.com>
2026-09-06 20:37:36 -0400

    Added JSON reference

--- SuperCorners/Config/ActionSet.swift
+++ SuperCorners/Config/ActionSet.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+struct ActionSet: Codable, Identifiable {
+    var id: String
+    var name: String
+    var targetBundleID: String?
+    var corners: CornerAssignments
+    var zones: ZoneAssignments
+}
+
+struct CornerAssignments: Codable {
+    var topLeft: ActionAssignment
+    var topRight: ActionAssignment
+    var bottomLeft: ActionAssignment
+    var bottomRight: ActionAssignment
+}
+
+struct ZoneAssignments: Codable {
+    var top: ActionAssignment
+    var left: ActionAssignment
+    var right: ActionAssignment
+    var bottom: ActionAssignment
+}
+
+struct ActionAssignment: Codable {
+    var actionID: String
+    var input: String?
+}
