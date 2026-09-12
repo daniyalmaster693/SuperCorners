01bbfcd4236a442d8853914ea06503381fa14906
Daniyal Master <daniyalmaster693@gmail.com>
2026-09-12 10:58:50 -0400

    Added a new activation manager class

--- SuperCorners/Actions/Activation.swift
+++ SuperCorners/Actions/Activation.swift
@@ -5,5 +5,17 @@
 //  Created by Daniyal Master on 2025-05-24.
 //
 
+import AppKit
 import KeyboardShortcuts
-import SwiftUI
+
+final class ActivationManager {
+    static let shared = ActivationManager()
+
+    private var mouseMonitor: Any?
+    private var clickMonitor: Any?
+    private var modifierFlags: Any?
+
+    private var currentFlags: NSEvent.ModifierFlags = []
+
+    private init() {}
+}
