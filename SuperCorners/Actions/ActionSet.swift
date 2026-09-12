1f79f3f0f6844999c6e0ec4d1a9f735fde418a4d
Daniyal Master <daniyalmaster693@gmail.com>
2026-09-12 14:14:03 -0400

    Updated default config

--- SuperCorners/Actions/ActionSet.swift
+++ SuperCorners/Actions/ActionSet.swift
@@ -44,7 +44,7 @@ struct SetActivation: Codable {
     var method: ActivationMethod
     var trigger: ActivationTrigger
     var modifierKey: ModifierKey?
-    var keyboardShortcut: String?
+    var keyboardShortcutName: String?
 }
 
 enum ModifierKey: String, Codable, CaseIterable, Identifiable {
